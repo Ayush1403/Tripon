3af9218866c219004374c350894d7b8605ed29e7
AyushSrivastava2793 <ayushsrivastava03004@gmail.com>
2025-10-20 00:08:40 +0530

    Update mail.js

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -4,25 +4,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 if (!process.env.RESEND_API) {
-  throw new Error("RESEND_API is required in .env");
+  throw new Error("RESEND_API key missing in .env");
 }
 
 export const resend = new Resend(process.env.RESEND_API);
 
-export const sendMail = async ({ to, subject, html, from }) => {
+export const sendMail = async({to , subject , html , from}) => {
   try {
-    const response = await resend.emails.send({
+    const response  = await resend.emails.send({
       from: from || "TripOn <noreply@ayushsrivastava.in>",
       to,
       subject,
       html,
-    });
-    
-    console.log("✅ Email Sent Successfully:", response.id);
+    })
+    console.log("Email Sent")
+
     return response;
   } catch (error) {
-    console.error("❌ Email Sending failed:");
-    console.error("Error:", error.message);
-    throw error;
+    console.log("Email Sending failed due to : " ,error);
   }
-};
+}
